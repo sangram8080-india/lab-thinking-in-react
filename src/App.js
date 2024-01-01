1dabaaadb6bcbb9c36d368fb12a0670686266e52
sangram <sangramgkp2020@gmail.com>
2024-01-02 01:04:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ProductPage from './components/ProductsPage';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ProductPage></ProductPage>
     </div>
   );
 }
